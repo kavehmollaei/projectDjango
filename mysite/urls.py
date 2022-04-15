b654f6ae35d863cf7e07f33732826cdec26e68a9
kavehmollaei <kavehmollaei@gmail.com>
2022-04-16 00:25:30 +0430

    add url empty for index of website application

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('website/',include('website.urls'))
+    path('',include('website.urls'))
    
 ]
--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,5 @@ from  website.views import http_test,json_response,index
 urlpatterns = [
 path('blog/',http_test),
 path('blog/json/',json_response),
-path('index',index),
+path('',index),
 ]
