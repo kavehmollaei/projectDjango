8ab6681a9d4afb681d01583bda3d318b3760ee94
kavehmollaei <kavehmollaei@gmail.com>
2022-04-26 23:36:07 +0430

    add custome admin pannel in admin.py in blog add

--- projectDjango/blog/admin.py
+++ projectDjango/blog/admin.py
@@ -4,10 +4,13 @@ from blog.models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    date_hierarchy='created_date'
-    empty_value_display='-empty-'
+   date_hierarchy='created_date'
+   empty_value_display='-empty-'
     # fields=('title',)
-    list_display =('title','counter_view','status')
+   list_display =('title','published_date','updated_time','status')
+   ordering=('updated_time',)
+   list_filter = ('status',)
+   search_fields=['content','title',]
 
 
 admin.site.register(Post,PostAdmin)
